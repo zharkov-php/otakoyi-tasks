cba727038a7eee9ae37a3640d1d51ebffa4c832a
Zharkov Andrey <zharkov.php@gmail.com>
2018-08-25 12:01:02 +0300

    Create class Router,it test, create autoload || Створив клас Router, протестив його та створив автозагрузку

---
+++ application/core/Router.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrey
+ * Date: 25.08.18
+ * Time: 11:41
+ */
+
+
+
+namespace application\core;
+
+
+class Router
+{
+
+
+    /**
+     * Router constructor.
+     * echo text
+     */
+    public function __construct()
+    {
+        echo 'I am class Router' . '<br>';
+    }
+
+
+    /**
+     * echo  text
+     */
+    public function run()
+    {
+        echo 'start method run';
+    }
+
+
+}
+
--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php
 
+require 'application/lib/Dev.php';
+
+use application\core\Router;
 
 
 echo '<br>' .'Привіт команда Отакої!!!' . '<br>';
+
+
+/**
+ * function autoload include class
+ * функція автозагрузки підключених класів
+ */
+spl_autoload_register(function($class) {
+    $path = str_replace('\\', '/', $class.'.php');
+    if (file_exists($path)) {
+        require $path;
+    }
+});
+
+
+session_start();
+
+$router = new Router;
+
+$router->run();
